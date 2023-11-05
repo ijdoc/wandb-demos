43d21cf33fa1512b35300ca1bf689a38c55822f4
J-Doc <jdoc@mail.com>
2023-11-05 14:07:16 -0500

    Install and configure the city lights theme

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # TODO: Check the output of nvidia-smi and exit if error
+pipenv run jupyter labextension install @yudai-nkt/jupyterlab_city-lights-theme
 pipenv run jupyter lab build
 pipenv run jupyter lab --ip=0.0.0.0 --no-browser --port 8888

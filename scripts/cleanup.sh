8a8916eb5f92da7ce33e736eac850e2d26506b7c
J-Doc <jdoc@mail.com>
2023-11-17 09:38:52 -0500

    Add images notebooks, refactor pytorch examples

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -15,11 +15,11 @@ pipenv sync --dev
 test_command_outcome "Clean root pipenv"
 
 # Find all .py files to reformat them
-find . -type f -name '*.py' ! -path './*/wandb/*' ! -path './*/node_modules/*' ! -path './*/artifacts/*' -exec pipenv run black {} \;
+find . -type f -name '*.py' ! -path './*/.wandb/*' ! -path './*/wandb/*' ! -path './*/node_modules/*' ! -path './*/artifacts/*' -exec pipenv run black {} \;
 test_command_outcome "Format python files"
 
 # Find all .ipynb files to clean and reformat them
-find . -type f -name '*.ipynb' ! -path './*/.ipynb_checkpoints/*' ! -path './*/wandb/*' ! -path './*/node_modules/*' ! -path './*/artifacts/*' -print0 | xargs -0 -I {} pipenv run jupyter nbconvert --clear-output --inplace {}
+find . -type f -name '*.ipynb' ! -path './*/.virtual_documents/*' ! -path './*/.wandb/*' ! -path './*/.ipynb_checkpoints/*' ! -path './*/wandb/*' ! -path './*/node_modules/*' ! -path './*/artifacts/*' -print0 | xargs -0 -I {} pipenv run jupyter nbconvert --clear-output --inplace {}
 test_command_outcome "Clear notebook outputs"
-find . -type f -name '*.ipynb' ! -path './*/.ipynb_checkpoints/*' ! -path './*/wandb/*' ! -path './*/node_modules/*' ! -path './*/artifacts/*' -print0 | xargs -0 -I {} pipenv run python scripts/format.py {}
+find . -type f -name '*.ipynb' ! -path './*/.virtual_documents/*' ! -path './*/.wandb/*' ! -path './*/.ipynb_checkpoints/*' ! -path './*/wandb/*' ! -path './*/node_modules/*' ! -path './*/artifacts/*' -print0 | xargs -0 -I {} pipenv run python scripts/format.py {}
 test_command_outcome "Format notebooks"
